9a33462de496e5fa66e6f612315831cec39d1157
melcary <melcary13@gmail.com>
2026-09-17 19:16:33 -0500

    assigment w03

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -7,10 +7,16 @@ const showCategoriesPage = async (req, res) => {
     res.render('categories', { title, categories });
 };  
 
-const showCategoryDetailsPage = async (req, res) => {
+const showCategoryDetailsPage = async (req, res, next) => {
     const categoryId = req.params.id;
 
     const category = await getCategoryById(categoryId);
+    if (!category) {
+        const err = new Error('Category not found');
+        err.status = 404;
+        return next(err);
+    }
+
     const projects = await getProjectsByCategoryId(categoryId);
 
     const title = 'Category Details';
