08e8941bbf159aafef05e3259a7d681ed70eb684
melcary <melcary13@gmail.com>
2026-09-22 17:30:12 -0500

    Insering new projects

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { showHomePage } from './controllers/index.js';
-import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
+import { showProjectsPage, showProjectDetailsPage, showNewProjectForm, processNewProjectForm, projectValidation} from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage} from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation, showEditOrganizationForm,  processEditOrganizationForm} from './controllers/organizations.js';
@@ -14,7 +14,9 @@ router.get('/categories', showCategoriesPage);
 router.get('/new-organization', showNewOrganizationForm);
 router.post('/new-organization', organizationValidation, processNewOrganizationForm);
 router.get('/edit-organization/:id', showEditOrganizationForm);
-router.post('/edit-organization/:id', organizationValidation,processEditOrganizationForm);
+router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
+router.get('/new-project', showNewProjectForm);
+router.post('/new-project',projectValidation, processNewProjectForm);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
