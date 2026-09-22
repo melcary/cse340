e7b36743a7a1a4ed53284c383205d4969fa0a301
melcary <melcary13@gmail.com>
2026-09-21 21:25:21 -0500

    Updating Data

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,4 +1,4 @@
-import { getAllOrganizations, getOrganizationDetails, createOrganization } from '../models/organizations.js';
+import { getAllOrganizations, getOrganizationDetails, createOrganization, updateOrganization} from '../models/organizations.js';
 import { getProjectsByOrganizationId } from '../models/projects.js';
 import { body, validationResult } from 'express-validator';
 
@@ -56,7 +56,7 @@ const processNewOrganizationForm = async (req, res) => {
         // Redirect back to the new organization form
         return res.redirect('/new-organization');
     }
-    
+
     const { name, description, contactEmail } = req.body;
     const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations
 
@@ -66,12 +66,42 @@ const processNewOrganizationForm = async (req, res) => {
     res.redirect(`/organization/${organizationId}`);
 };
 
+const showEditOrganizationForm = async (req, res) => {
+    const organizationId = req.params.id;
+    const organizationDetails = await getOrganizationDetails(organizationId);
 
+    const title = 'Edit Organization';
+    res.render('edit-organization', { title, organizationDetails });
+};
+
+const processEditOrganizationForm = async (req, res) => {
+    const results = validationResult(req);
+    if (!results.isEmpty()) {
+    // Validation failed - loop through errors
+    results.array().forEach((error) => {
+        req.flash('error', error.msg);
+    });
+
+    // Redirect back to the edit organization form
+    return res.redirect('/edit-organization/' + req.params.id);
+    }
+    const organizationId = req.params.id;
+    const { name, description, contactEmail, logoFilename } = req.body;
+
+    await updateOrganization(organizationId, name, description, contactEmail, logoFilename);
+    
+    // Set a success flash message
+    req.flash('success', 'Organization updated successfully!');
+
+    res.redirect(`/organization/${organizationId}`);
+};
 
 export {
     showOrganizationsPage,
     showOrganizationDetailsPage,
     showNewOrganizationForm,
     processNewOrganizationForm,
-    organizationValidation
+    organizationValidation,
+    showEditOrganizationForm,
+    processEditOrganizationForm 
 };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { showHomePage } from './controllers/index.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage} from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
-import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation} from './controllers/organizations.js';
+import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation, showEditOrganizationForm,  processEditOrganizationForm} from './controllers/organizations.js';
 
 const router = express.Router();
 
@@ -12,7 +12,9 @@ router.get('/organizations', showOrganizationsPage);
 router.get('/projects', showProjectsPage);
 router.get('/categories', showCategoriesPage);
 router.get('/new-organization', showNewOrganizationForm);
-router.post('/new-organization',organizationValidation, processNewOrganizationForm);
+router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+router.get('/edit-organization/:id', showEditOrganizationForm);
+router.post('/edit-organization/:id', organizationValidation,processEditOrganizationForm);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
