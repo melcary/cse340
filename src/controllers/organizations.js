bcd90e7d6d72c22cb6cfdc2d12dda753954bf46d
melcary <melcary13@gmail.com>
2026-09-21 20:24:54 -0500

    sessions and flash

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -27,7 +27,11 @@ const processNewOrganizationForm = async (req, res) => {
     const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations
 
     const organizationId = await createOrganization(name, description, contactEmail, logoFilename);
+    req.flash('success', 'Organization added successfully!')
+    
     res.redirect(`/organization/${organizationId}`);
 };
+
+
 
 export { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm };
